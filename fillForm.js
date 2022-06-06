400eb180d3ed2ef463bf8d272a3bc07babd13952
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-06-06 15:34:40 +0530

    Added greet to fill form

--- fillForm.js
+++ fillForm.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 const captureName = (name, formEntry) => { formEntry.name = name; };
 
 const captureDOB = (DOB, formEntry) => {
-  const dob = DOB.split('-').map(datePart => parseInt(datePart));
+  console.log(DOB);
+  const dob = DOB.split(' ').map(datePart => parseInt(datePart));
   formEntry.DOB = dob;
 };
 
 const captureHobbies = (hobbies, formEntry) => {
   const hobbiesList = hobbies.split(',');
   formEntry.hobbies = hobbiesList;
+  fillForm(formEntry);
 };
 
-const fillForm = (entry, formFile) => {
-  fs.writeFileSync(formFile, entry, 'utf8');
-  process.exit(0);
+const fillForm = (entry) => {
+  const formFile = './formsData.json'; 
+  fs.writeFileSync(formFile, JSON.stringify(entry), 'utf8');
 };
 
 const main = () => {
@@ -26,26 +28,30 @@ const main = () => {
     },
     {
       capture: captureDOB,
-      message: 'Please Enter Your DOB (YYYY-MM-DD): '
+      message: 'Please Enter Your DOB (YYYY MM DD): '
     },
     {
       capture: captureHobbies,
       message: 'Please Enter Your Hobbies : '
     },
     {
-      capture: fillForm,
+      capture: () => process.exit(0),
       message: 'Thank You'
-    },
+    }
   ];
   
   let currentField = 0;
   let { capture, message } = fieldData[currentField];
   console.log(message);
   
+  process.stdin.setEncoding('utf8');
   process.stdin.on('data', (cell) => {
     capture(cell, formEntry);
     currentField++;
     
+    capture = fieldData[currentField].capture;
+    message = fieldData[currentField].message;
+    console.log(message);
   });
 };
 

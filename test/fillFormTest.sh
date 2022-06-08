b0f930eca83e2e77dad969a582feda9bb0c68e27
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-06-08 18:28:35 +0530

    refactored stdin level test

--- test/fillFormTest.sh
+++ test/fillFormTest.sh
@@ -1,14 +1,29 @@
 #! /bin/bash
-responses='Suresh\n1234-12-12\nsome,hobbies\n1234567890\nadd1\nadd2';
 testName=' fillForm - should fill form'
+responses='Suresh\n1234-12-12\nsome,hobbies\n1234567890\nadd1\nadd2';
+testContent='{
+  "name": "Suresh",
+  "dob": "1234-12-12",
+  "hobbies": [
+    "some",
+    "hobbies"
+  ],
+  "phone_number": "1234567890",
+  "address": "add1\nadd2"
+}'
 
+expectedForm=`mktemp`
+
+echo -n "${testContent}" > "${expectedForm}"
 node "../fillForm.js" <<< "${responses}" &> /dev/null;
-diff "./test/expectedForm" "./forms.json" &> /dev/null ;
 
-if [[ $? -eq "0" ]];
+diff "${expectedForm}" "./forms.json" #&> /dev/null ;
+assertStatus=$?;
+
+status="❌:${testName}";
+if [[ "${assertStatus}" -eq "0" ]];
 then
-  echo "✅:${testName}";
-  exit
+  status="✅:${testName}";
 fi
 
-echo "❌:${testName}"
+echo "${status}"
